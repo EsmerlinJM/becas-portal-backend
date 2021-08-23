7e56b9c7b0e77ad2f5efead733cdcb87699a5b9f
Gustavo Valverde <gustavo@iterativo.do>
2021-08-23 18:07:40 -0400

    fix: use env variables for passport

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 if [ -z "$FRESH_INSTALL" ]; then
     echo "The variable \$FRESH_INSTALL is empty, using default init"
     php artisan migrate
-    php artisan passport:install
 else
     php artisan migrate:refresh
     php artisan db:seed
