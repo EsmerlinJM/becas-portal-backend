2144159cbb0dfedf9f6dcda639d08911015466bf
Ivan Firestone <ivan.firestone@icloud.com>
2021-09-10 13:57:41 -0400

    Contestar Pregunta Formulario

---
+++ app/Http/Controllers/SocioEconomicoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SocioEconomico;
+use Illuminate\Http\Request;
+
+class SocioEconomicoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SocioEconomico  $socioEconomico
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SocioEconomico $socioEconomico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SocioEconomico  $socioEconomico
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SocioEconomico $socioEconomico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SocioEconomico  $socioEconomico
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SocioEconomico $socioEconomico)
+    {
+        //
+    }
+}
