e1774278a38898a31c1e1d43685dfd8e9a0aeefd
Ivan Firestone <ivan.firestone@icloud.com>
2021-09-11 23:19:34 -0400

    Emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-{{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
-<img src="{{ asset('Logo_BECATUFUTURO.png') }}" alt="Laravel Logo">
+{{-- <img src="https://storage.googleapis.com/becas-backend-storage/APP_IMAGES/Logo_BECATUFUTURO_BIG.png" alt="Beca Tu Futuro Logo"> --}}
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
